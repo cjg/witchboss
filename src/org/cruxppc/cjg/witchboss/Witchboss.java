33ed17f1a7fc8584b56a51f75e14602b75cf78e8
Giuseppe Coviello <cjg@cruxppc.org>
2009-07-18 10:24:04 +0200

    initial import

---
+++ src/org/cruxppc/cjg/witchboss/Witchboss.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.cruxppc.cjg.witchboss;
+
+/**
+ * @author cjg
+ *
+ */
+public class Witchboss {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Witchboss!");
+	}
+
+}
